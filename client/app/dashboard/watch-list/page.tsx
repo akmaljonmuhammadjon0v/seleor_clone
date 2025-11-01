b7ea15bc55524b7499ea329837a92a5c86a3b68a
akmaljon <akmal@gmail.com>
2025-11-01 19:41:47 +0500

    №16. User transaction data

--- client/app/dashboard/watch-list/page.tsx
+++ client/app/dashboard/watch-list/page.tsx
@@ -1,29 +1,51 @@
+import { getFavourites } from '@/actions/user.action';
 import WatchListCard from '@/components/card/watch-list.card';
 import Filter from '@/components/shared/filter';
 import Pagination from '@/components/shared/pagination';
 import { Separator } from '@/components/ui/separator';
-import { products } from '@/constants';
+import { SearchParams } from '@/types';
+import React, { FC } from 'react';
 
-import React from 'react';
+interface Props {
+	searchParams: SearchParams;
+}
+const Page: FC<Props> = async props => {
+	const searchParams = await props.searchParams;
+	const res = await getFavourites({
+		searchQuery: `${searchParams.q || ''}`,
+		filter: `${searchParams.filter || ''}`,
+		page: `${searchParams.page || '1'}`,
+		category: `${searchParams.category || ''}`,
+	});
 
-const WatchList = () => {
+	const products = res?.data?.products;
+	const isNext = res?.data?.isNext || false;
+
 	return (
 		<>
-			<h1 className='text-xl font-bold'>Watch List</h1>
+			<h1 className='text-xl font-bold'>Watch list</h1>
 
 			<Separator className='my-3' />
 
 			<Filter showCategory />
 
+			{products && products.length === 0 && (
+				<div className='text-center mt-3 '>No products found.</div>
+			)}
+
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-3'>
-				{products.map(product => (
-					<WatchListCard key={product._id} product={product} />
-				))}
+				{products &&
+					products.map(product => (
+						<WatchListCard key={product._id} product={product} />
+					))}
 			</div>
 
-			<Pagination />
+			<Pagination
+				isNext={isNext}
+				pageNumber={searchParams?.page ? +searchParams.page : 1}
+			/>
 		</>
 	);
 };
 
-export default WatchList;
+export default Page;
